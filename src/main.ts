949305ac1aeb76de5027dee3e16844751f0030f2
Stefan Sträter <stefan.straeter@gmail.com>
2026-05-31 13:40:55 +0200

    Remove commented-out function showScreen from main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,11 @@
 import './styles/main.scss';
+
+
+/*
+function showScreen(screenId: string) {
+  // Alle Screens hiden
+  document.querySelectorAll('.screen').forEach(s => s.classList.add('u-hidden'));
+  // Gewünschten Screen zeigen
+  document.getElementById(screenId)?.classList.remove('u-hidden');
+}
+  */
