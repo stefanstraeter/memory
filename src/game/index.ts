a24272506bdb57d3eadcff9daa4ffa9cc5f4d95f
Stefan Sträter <stefan.straeter@gmail.com>
2026-06-10 14:41:06 +0200

    feat: add index file to export game and UI related functions

---
+++ src/game/index.ts
@@ -0,0 +1,11 @@
+export { initGame } from './game';
+export { getWinner, getWinnerLabel, getDrawIcon, getPlayerIcon, getThemeFolder, getWinnerIcon, createCardValues } from './helpers';
+export {
+  buildHeader,
+  setupExitModal,
+  buildGrid,
+  updateHeader,
+  updateGameOverScreen,
+  updateWinnerScreen,
+  updateDrawScreen,
+} from './gameUi';
