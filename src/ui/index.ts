0238641ea654df4480d9177d9bcea1632dd2a139
Stefan Sträter <stefan.straeter@gmail.com>
2026-06-10 14:41:33 +0200

    feat: create index file to export UI and theme functionalities

---
+++ src/ui/index.ts
@@ -0,0 +1,3 @@
+export { initUI, showScreen } from './ui';
+export { getActiveTheme, applyEndScreenButtonLabels, applyEndScreenThemeAssets, syncEndScreenTheme, setupThemeSyncObserver } from './theme';
+export { themePreviews } from './dom';
