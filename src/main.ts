45f526816dcc927377105c3b6615c0d71bf819c2
Stefan Sträter <stefan.straeter@gmail.com>
2026-06-12 09:43:33 +0200

    refactor: remove unnecessary newline in main.ts for cleaner code

--- src/main.ts
+++ src/main.ts
@@ -5,5 +5,4 @@ import { showHomeScreen } from './ui/navigation';
 /**
  * The entry point of the application. It initializes the user interface once the DOM content is fully loaded.
  */
-
 document.addEventListener('DOMContentLoaded', showHomeScreen);
